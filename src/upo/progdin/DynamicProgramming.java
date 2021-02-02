e8baa6471b01abaa196c1803310d822b1ea975a6
Lorenzo Rossiman <20031485@studenti.uniupo.it>
2021-02-02 23:47:47 +0100

    aggiunto Knapsack01

--- src/upo/progdin/DynamicProgramming.java
+++ src/upo/progdin/DynamicProgramming.java
@@ -1,5 +1,7 @@
 package upo.progdin;
 
+import java.util.Arrays;
+
 public class DynamicProgramming {
 	
 	/** Calcola la LCS tra <code>s1</code> e <code>s2</code> utilizzando l'algoritmo visto a lezione.
@@ -24,13 +26,56 @@ public class DynamicProgramming {
 	 * incluso nella soluzione, false altrimenti.
 	 */
 	public static boolean[] getKnapsack01(int[] weights, int[] values, int maxWeight) {
-		throw new UnsupportedOperationException("Not yet implemented");
-//		 _____ ___  ____   ___  
-//		 |_   _/ _ \|  _ \ / _ \ 
-//		   | || | | | | | | | | |
-//		   | || |_| | |_| | |_| |
-//		   |_| \___/|____/ \___/ 
-//		                         
+		boolean[] solution = new boolean[weights.length + 1];
+		Arrays.fill(solution, false);
+		
+		int n = weights.length;
+		int[][] V = new int[n + 1][maxWeight + 1];
+		int[][] K = new int[n + 1][maxWeight + 1];
+		
+		//initialize - fill matrices' first column with zeros
+		for(int i = 0; i <= n; ++i) {
+			V[i][0] = 0;
+			K[i][0] = 0;
+		}
+		
+		//initialize - fill matrices' first row with zeros
+		for(int j = 0; j <= maxWeight; ++j) {
+			V[0][j] = 0;
+			K[0][j] = 0;
+		}
+		
+		//fill the sub-matrices (old matrices except first row and first column)
+		for(int i = 1; i <= n; ++i) {
+			for(int j = 1; j <= maxWeight; ++j) {
+
+				if(j < weights[i - 1]) {
+					V[i][j] = V[i - 1][j];
+					K[i][j] = 0;
+				}
+				
+				//else V[i, j] = max(V[i-1, j],  V[i-1, j-p[i]] + v[i])
+				else if(V[i - 1][j] >= V[i - 1][j - weights[i - 1]] + values[i - 1]) 
+					V[i][j] = V[i - 1][j];
+				
+				else{
+					V[i][j] = V[i - 1][j - weights[i - 1]] + values[i - 1];
+					K[i][j] = 1;
+				}
+			}
+		}
+		
+		//fill solution
+		int d = maxWeight;
+		
+		for(int i = n; i > 0; --i) {
+			if(K[i][d] == 1) {
+				solution[i] = true;
+				d -= weights[i];
+			}
+		}
+		
+		return solution;
 	}
 	
 }
--- src/upo/progdin/DynamicProgrammingTest.java
+++ src/upo/progdin/DynamicProgrammingTest.java
@@ -1,14 +1,34 @@
 package upo.progdin;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class DynamicProgrammingTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		int maxWeight = 10;
+		
+		int[] weights = new int[4];
+		int[] values = new int[4];
+		
+		weights[0] = 2;
+		values[0] = 12;
+		
+		weights[1] = 7;
+		values[1] = 6;
+		
+		weights[2] = 6;
+		values[2] = 2;
+		
+		weights[3] = 4;
+		values[3] = 1;
+		
+		boolean[] solution = DynamicProgramming.getKnapsack01(weights, values, maxWeight);
+		
+		System.out.println("Solutions:\n{");
+		for(int i = 1; i < solution.length; ++i)
+			System.out.println("\tx_" + i + ": " + solution[i]);
+		System.out.println("}");
 	}
 
 }
